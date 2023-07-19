34446c881c8b0cb88e2b42f69217b92abe0ccef6
catsoba <yuuking.ogu.2004@gmail.com>
2023-07-19 19:33:20 +0900

    うごくよ

---
+++ Assets/MoveC.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveC : MonoBehaviour
+{
+
+    public float speed = 0.01f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 pos = this.transform.position;
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            pos.x += speed;
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            pos.x -= speed;
+        }
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            pos.y += speed;
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            pos.y -= speed;
+        }
+        
+        this.transform.position = pos;
+    }
+}
